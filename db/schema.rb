9909df982794363397c65385cc161bdea24b13e1
Rodrigo Vilar <rodrigovilar@dcx.ufpb.br>
2017-06-03 14:20:24 -0300

    Generating Student scaffold with template customization

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602000656) do
+ActiveRecord::Schema.define(version: 20170603171455) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
